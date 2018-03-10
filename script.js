c46f2e9b63226ef820ed2f74c57c456d395cecf4
cindyh127 <2020cindyh@chapin.edu>
2018-03-10 12:08:51 -0500

    First-Commit

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 /* remove this comment and place your JavaScript here */ 
+/*
+When did you start studying for exams?
+a. 1 month in advance of course(1pt)
+b. I start organzing 2 weeks in advance and study the week before(2pt)
+c. I'll probably pull an all nighter before each one(3pt)
+d. WHAT EXAMS(4pt)
+
+What do you see yourself doing the night before the exam?
+a. just reviewing everything I've been studying (1pt)
+b. Color coding a schedule for after exams(2pt)
+c. I'm beginning my all nighter(3pt)
+d. WHAT EXAMS,I've already fallen asleep(4pt)
+
+What do you ask the teacher at a review session?
+
+a. I have a list of well thought out and specific questions that I've already asked three other people about(1pt)
+b. the best way to organize for exams(2pt)
+c. how much studying is necessary( i.e. how many hours do I have to study minimum)(3pt)
+d. HAha YoU ThINK i WouLd Go To A STudY sESSion(-_-)(4pt)
+
+
+
+3-4pt:rachel
+
+5-7pt:roygbiv
+
+8-9pt:procrastination
+
+10-12pt:he-who-must-not-be-named
+*/
+
+
+
+
+
+
